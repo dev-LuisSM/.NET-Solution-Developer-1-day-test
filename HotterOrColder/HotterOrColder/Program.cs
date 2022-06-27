2c470c142689a84bfb4907222d191bddb61231d2
Luis Silva Martin <72401861+dev-LuisSM@users.noreply.github.com>
2022-06-27 18:28:03 -0500

    Add : Hotter or Colder

---
+++ HotterOrColder/HotterOrColder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotterOrColder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Random rdm = new Random();
+			int randomNumber = rdm.Next(1, 100);
+
+			Console.WriteLine("I'm thinking of a random number between 1 and 100, can you guess what it is?");
+			int guessNumber = 0;
+			int tries = 0;
+
+			while (guessNumber != randomNumber)
+			{
+
+				Console.Write("Write here your number: ");
+				string getNumber = Console.ReadLine();
+
+				if (!int.TryParse(getNumber, out guessNumber))
+				{
+					Console.WriteLine("That's not a number \n");
+					continue;
+				}
+
+				if (guessNumber > randomNumber)
+					Console.WriteLine("Hotter \n");
+
+				else if (guessNumber < randomNumber)
+					Console.WriteLine("Colder \n");
+
+				else if (guessNumber == randomNumber)
+					Console.WriteLine("That's the number, it tooked you " + tries + " tries!");
+
+				tries++;
+			};
+			Console.ReadLine();
+		}
+    }
+}
